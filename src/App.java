796bbec6b3d045a5094b934c81bc58866a0282b8
monster <monster@Monster>
2023-10-19 00:08:12 -0600

    Conexion y agregar la libreria de conexion

--- src/App.java
+++ src/App.java
@@ -1,17 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-
-/**
- *
- * @author monster
- */
 public class App {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // Va a ser solo un test para ver como carga todo en github
         System.out.print("Hola mundo!");
---
+++ src/Modelo/Conexion.java
@@ -0,0 +1,43 @@
+package Modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+    private static Conexion instance;
+    private Connection con;
+
+    private String base = "autos";
+    private String user = "root";
+    private String password = "";
+    private String url = "jdbc:mysql://localhost:3306/" + base;
+
+    private Conexion() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(url, user, password);
+            System.out.print("Conexion Exitosa");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.print("Error al conectar la base de datos");
+            System.out.print(e.getCause());
+            System.out.print(e.getMessage());
+        }
+    }
+
+    public static Conexion getInstance() {
+        if (instance == null) {
+            synchronized (Conexion.class) {
+                if (instance == null) {
+                    instance = new Conexion();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public Connection getConexion() {
+        return con;
+    }
+}
